3af902a0f76ff47dfd79d483e280e0ab64699435
cherishwish <cherishwish12@gmail.com>
2025-05-24 17:26:29 +0900

    유저관련기본세팅

---
+++ src/main/java/com/example/demo/converter/UserConverter.java
@@ -0,0 +1,19 @@
+package com.example.demo.converter;
+
+import com.example.demo.domain.UserEntity;
+import com.example.demo.dto.UserDto;
+
+public class UserConverter {
+    public static UserDto toDto(UserEntity entity) {
+        UserDto dto = new UserDto();
+        dto.setId(entity.getId());
+        dto.setNickname(entity.getNickname());
+        dto.setProfileImage(entity.getProfileImage());
+        return dto;
+    }
+
+    public static void updateEntity(UserEntity entity, UserDto dto) {
+        entity.setNickname(dto.getNickname());
+        entity.setProfileImage(dto.getProfileImage());
+    }
+}
