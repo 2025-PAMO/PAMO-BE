675aeb0fc55e3f65f1c4c537655b611e67509af4
cherishwish <cherishwish12@gmail.com>
2025-05-26 20:50:03 +0900

    Refactor: 메인 브랜치 폴더구조 수정

--- src/main/java/com/example/demo/Repository/BaseMusicRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Entity.BaseMusic;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BaseMusicRepository extends JpaRepository<BaseMusic, Integer> {
-}
-
--- src/main/java/com/example/demo/dto/ChatRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dto;
-
-import lombok.Data;
-
-@Data
-public class ChatRequestDto {
-    private Integer userId;
-    private String question;
-}
