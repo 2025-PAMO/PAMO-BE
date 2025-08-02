f18379fd93aeaffe49ca1c67499fd2b7bf5dd3e0
soyoon1 <20210841@duksung.ac.kr>
2025-08-02 23:10:46 +0900

    Feat: 마이페이지 - 라이브러리 API 구현 #10

--- src/main/java/com/example/demo/controller/MyPageController.java
+++ src/main/java/com/example/demo/controller/MyPageController.java
@@ -2,7 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.apiPayload.CustomResponse;
 import com.example.demo.dto.myMusic.MyMusicResponseDTO;
-import com.example.demo.service.MyMusicService;
+import com.example.demo.service.MyPageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,14 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/mypage")
 @RequiredArgsConstructor
 public class MyPageController {
-    private final MyMusicService myMusicService;
+    private final MyPageService myPageService;
 
     @GetMapping("/musics")
     public CustomResponse<MyMusicResponseDTO> getMyMusic(
             @RequestParam Integer id,
             @RequestParam String type
     ) {
-        MyMusicResponseDTO result = myMusicService.getMyMusic(id, type);
+        MyMusicResponseDTO result = myPageService.getMyMusic(id, type);
+        return CustomResponse.onSuccess(result);
+    }
+
+    @GetMapping("/library")
+    public CustomResponse<MyMusicResponseDTO> getMyLibrary(
+            @RequestParam Integer id,
+            @RequestParam String type
+    ){
+        MyMusicResponseDTO result = myPageService.getMyLibrary(id, type);
         return CustomResponse.onSuccess(result);
     }
 
