e5270242b71f68cb82879acdde265d11153ce1b8
cherishwish <cherishwish12@gmail.com>
2025-08-12 01:11:02 +0900

    Fix: User JoinType 필드 삭제 및 DB초기화 코드 수정 #12

--- src/main/java/com/example/demo/repository/MotionMusicRepository.java
+++ src/main/java/com/example/demo/repository/MotionMusicRepository.java
@@ -6,6 +6,7 @@ import com.example.demo.repository.projection.MusicSearchProjection;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
